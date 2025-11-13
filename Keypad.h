dab97b86e4d49a5a7aa63f94b4c5160bc776e58d
rokia <re2301199@tkh.edu.eg>
2025-11-14 00:41:16 +0200

    Add Keypad.h file

---
+++ Keypad.h
@@ -0,0 +1,12 @@
+#ifndef __KEYPAD__
+#define __KEYPAD__
+
+#define NO_KEY     0      
+#define UP_KEY     1     
+#define DOWN_KEY   2      
+#define SELECT_KEY 3      
+
+
+unsigned char Read_Keypad(void);
+
+#endif
