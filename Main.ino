11c435daae68168bd93cad2d69772a016e0852f7
rokia <re2301199@tkh.edu.eg>
2025-11-15 18:45:54 +0200

    Overwrite Main.ino with my version from HAL

--- Main.ino
+++ Main.ino
@@ -6,21 +6,20 @@
 #include "Dio.h"
 #include "Keypad.h"
 
-#define MAX_LIMIT 1023
+#define MAX_LIMIT 1023  
 #define MIN_LIMIT 0
 
-unsigned short LL = 200;
-unsigned short LH = 900;
-unsigned short LDR_Value;
-
-unsigned char editMode = 0;   // 0=normal, 1=edit LL, 2=edit LH
-unsigned char modeTimer = 0;
+unsigned short LL = 200;      
+unsigned short LH = 900;      
+unsigned short LDR_Value;    
+unsigned char editMode = 0;   
+unsigned char modeTimer = 0;  
 
 int main(void)
 {
-    LCD_Init();
-    Adc_Init();
-    DIO_SetPinDirection('D', 2, 'o'); // PD2 as output (buzzer)
+    LCD_Init();                        
+    Adc_Init();                        
+    DIO_SetPinDirection('D', 2, 'o');  
 
     LCD_Clear();
     LCD_String_xy(0,0,"LDR MONITOR READY");
@@ -29,5 +28,55 @@ int main(void)
 
     while(1)
     {
-    }
+        LDR_Value = Adc_ReadChannel(1);
+        unsigned char KeyPressed = Read_Keypad();
+
+        if (KeyPressed == SELECT_KEY)
+        {
+            editMode++;
+            if (editMode > 2) editMode = 0;  
+            modeTimer = 0;                    
+            _delay_ms(300);                   
+        }
+
+        // LL
+        if (editMode == 1)
+        {
+            if (KeyPressed == UP_KEY && LL < MAX_LIMIT - 10) { LL += 10; modeTimer = 0; }    
+            if (KeyPressed == DOWN_KEY && LL > MIN_LIMIT + 10) { LL -= 10; modeTimer = 0; }  
+        }
+        // LH 
+        else if (editMode == 2)
+        {
+            if (KeyPressed == UP_KEY && LH < MAX_LIMIT - 10) { LH += 10; modeTimer = 0; }    
+            if (KeyPressed == DOWN_KEY && LH > MIN_LIMIT + 10) { LH -= 10; modeTimer = 0; }  
+        }
+
+        // exit after ~3 seconds (15×200ms) 
+
+        if (editMode != 0)       
+        {
+            modeTimer++;
+            if (modeTimer > 15)  
+            {
+                editMode = 0;    
+                modeTimer = 0;   
+            }
+        }
+
+        // If outside range, buzzer ON; otherwise OFF
+        unsigned char buzState = 0;
+        if (LDR_Value < LL || LDR_Value > LH)
+        {
+            DIO_SetPinState('D', 2, 'h');  
+            buzState = 1;
+        }
+        else
+        {
+            DIO_SetPinState('D', 2, 'l');  
+            buzState = 0;
+        }
+
+       
 }
+
